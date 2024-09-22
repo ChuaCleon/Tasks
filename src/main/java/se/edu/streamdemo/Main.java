6cf21f422b03d0390e23a06946f3a02cc39c56c4
ChuaCleon <chuaweiqian.cleon@gmail.com>
2024-09-22 20:59:35 +0800

    Add method to print using stream

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -41,6 +41,9 @@ public class Main {
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasks){
+        System.out.println("Printing data with stream");
+        tasks.stream() // Creating a stream
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
